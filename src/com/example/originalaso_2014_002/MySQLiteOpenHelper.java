90fc6585796745a20a53d6575f493e655ade642b
student <student@PC111S37.fgakusei.local>
2014-06-25 11:10:02 +0900

    6/25

--- src/com/example/originalaso_2014_002/MySQLiteOpenHelper.java
+++ src/com/example/originalaso_2014_002/MySQLiteOpenHelper.java
@@ -1,7 +1,12 @@
 package com.example.originalaso_2014_002;
 import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
-public class MySQLiteOpenHelper{
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper{
 	public MySQLiteOpenHelper(Context context){
 		super(context, "20140021201755.sqlite3", null, 1);
 	}
@@ -9,8 +14,41 @@ public class MySQLiteOpenHelper{
 		db.execSQL("CREATE TABLE IF NOT EXISTS"+
 	"Hitokoto(_id INTEGER PRIMARY KEY AUTOINCREMENT NOT MULL,phrase TEXT)");
 	}
+	
 	public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
 		db.execSQL("drop table Hitokoto");
 		onCreate(db);
+	}
+	public void insertHitokoto(SQLiteDatabase db,String inputMsg){
+		String sqlstr = "insert into Hitokoto(phrase)values(`"+inputMsg+"`);";
+		try{
+			db.beginTransaction();
+			db.execSQL(sqlstr);
+			db.setTransactionSuccessful();
+		} catch (SQLException e){
+			Log.e("ERROR",e.toString());
+		}finally {
+			db.endTransaction();
+		}
+		return;
+	}
+	public String selectRandomHitokoto(SQLiteDatabase db){
+		
+		String rtString = null;
+		
+		String sqlst = "SELECT_id,phrase From Hitokoto ORDER BY RANDOM();";
+		try{
+			SQLiteCursor cursor = (SQLiteCursor)db.rawQuery(sqlst,null);
+			if(cursor.getCount()!=0){
+				cursor.moveToFirst();
+				rtString = cursor.getString(1);
+			}
+			cursor.close();
+		} catch(SQLException e){
+			Log.e("ERROR",e.toString());
+		}finally{
+			
+		}
+		return rtString;
 	}
 }
