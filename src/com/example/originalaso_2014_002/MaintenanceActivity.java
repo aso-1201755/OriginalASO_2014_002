5e99e5365e18be7f41559895cfeeb51005c4c3f1
student <student@PC111S37.fgakusei.local>
2014-07-28 14:39:36 +0900

    7/28

--- src/com/example/originalaso_2014_002/MaintenanceActivity.java
+++ src/com/example/originalaso_2014_002/MaintenanceActivity.java
@@ -12,7 +12,8 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
-public class MaintenanceActivity extends Activity implements View.OnClickListener,AdapterView.OnItemClickListene
+
+public class MaintenanceActivity extends Activity implements View.OnClickListener,AdapterView.OnItemClickListener{
 SQLiteDatabase sdb = null;
 
 MySQLiteOpenHelper helper = null;
@@ -21,16 +22,24 @@ int selectedID = -1;
 
 int lastPosition = -1;
 
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO 自動生成されたメソッド・スタブ
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.maintenance);
+}
+private void setDBValuetoList(ListView lstHitokoto){
+	
 	SQLiteCursor cursor = null;
 
 	if(sdb == null){
-		helper = new MySQliteOpenHelper(getApplicationContext());
+		helper = new MySQLiteOpenHelper(getApplicationContext());
 	}
 	try{
 		sdb = helper.getWritableDatabase();
-	}catch(SQLiteEception e){
+	}catch(SQLiteException e){
 
-		Log.e("EROOER",e.toString);
+		Log.e("ERROR",e.toString());
 	}
 	cursor = this.helper.selectHitokotoList(sdb);
 
@@ -40,10 +49,23 @@ int lastPosition = -1;
 
 	int[]to = new int[]{android.R.id.text1};
 
-	SimpleCursorAdapter adaptor =
-	  newSimpleCursorAdapter(this,db_layout,cursor,from,to,0);
+	SimpleCursorAdapter adapter =
+	  new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
 
 	lstHitokoto.setAdapter(adapter);
+}
+
+private void deleteFromHitokoto(int id){
+	
+	if(sdb == null){
+		helper = new MySQLiteOpenHelper(getApplicationContext());
+	}
+	try{
+		sdb = helper.getWritableDatabase();
+	}catch(SQLiteException e){
+		Log.e("ERROR",e.toString());
+	}
+	this.helper.deleteHitokoto(sdb, id);
 }
 	@Override
 	protected void onResume(){
@@ -61,5 +83,35 @@ int lastPosition = -1;
 
 		this.setDBValuetoList(lstHitokoto);
 
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()){
+		case R.id.btnDELETE:
+			
+			if(this.selectedID != -1){
+				this.deleteFromHitokoto(this.selectedID);
+				ListView lstHitokoto = (ListView)findViewById(R.id.LyHITOKOTO);
+				
+				this.setDBValuetoList(lstHitokoto);
+				
+				this.selectedID =-1;
+				this.lastPosition = -1;	
+			}
+			else{
+				Toast.makeText(MaintenanceActivity.this,"削除する行を選んでください",Toast.LENGTH_SHORT).show();
+			}
+			break;
+		case R.id.btnMAINTE_BACk:
+		finish();
+		break;
+		
+		}
+	}
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// TODO 自動生成されたメソッド・スタブ
+		
 	}
 }
